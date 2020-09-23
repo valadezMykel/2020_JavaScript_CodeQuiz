4ab99b461dd712b57dc1cf05289828088475ba42
Mykel Valadez <valadezmykel@gmail.com>
2020-09-23 13:03:10 -0700

    timer update

--- assets/JavaScript/main.js
+++ assets/JavaScript/main.js
@@ -1,22 +1,40 @@
 
 var starterBlockEl = document.getElementById("starterBlock");
 
-var timer = 30;
+var timer = 5;
 
-starterBlockEl.children[1].addEventListener("click", function(){
+document.getElementById("startBtn").addEventListener("click", function(){
     // removes the start button
     starterBlockEl.removeChild(starterBlockEl.children[1]);
     // starts the timer
     var timerInterval = setInterval(function(){
-        starterBlockEl.children[0].textContent = timer + "seconds remain";
+        starterBlockEl.children[0].textContent = timer + " seconds remain";
         timer--;
 
-        if(timer < 0){
+        if(timer <= 0){
             clearInterval(timerInterval)
-            starterBlockerEl.textContent = "You have failed the quiz!!"
+            placeStartBtn()
+            timer = 5;
+            starterBlockEl.children[0].textContent = "You have failed the quiz!!"
+
         }
 
         
     }, 1000)
 
 });
+
+function quizGame {
+    
+}
+
+
+
+function placeStartBtn(){
+    let startBtnEl = document.createElement("button")
+    startBtnEl.textContent = "start";
+    startBtnEl.setAttribute("class", "bg-dark");
+    startBtnEl.setAttribute("id", "startBtn");
+    starterBlockEl.appendChild(startBtnEl);
+
+}
