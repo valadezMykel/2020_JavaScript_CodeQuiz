08fd874d9de107f9a3d35e74bc04a9df328da39f
Mykel Valadez <valadezmykel@gmail.com>
2020-09-27 12:23:15 -0700

    answer buttons now remove after one is clicked

--- assets/JavaScript/main.js
+++ assets/JavaScript/main.js
@@ -3,11 +3,13 @@ var starterBlockEl = document.getElementById("starterBlock");
 var cardTextEl = document.getElementById("the-card-text");
 var cardEl = document.getElementById("the-card");
 
-var question1 = {quest: "Who are you?", answers: ["you", "they", "me", "we"], correctAnswer: 2 }
-var questionObjectArr = [question1] 
+var question1 = {quest: "Who are you?", answers: ["you", "they", "me", "we"], correctAnswer: 2 };
+let question2 ={quest: "How many days was JavaScript made in?", answers: ["7", "10", "18", "35"], correctAnswer: 1 };
+var questionObjectArr = [question1, question2]; 
 
 var timer = 5;
 let currentQuestion = 0;
+let score = 0;
 
 // listens at the start button to start the game timer
 document.getElementById("startBtn").addEventListener("click", runTimer);
@@ -33,17 +35,30 @@ function runTimer(){
 }
 
 function quizGameRound() {
+    if(currentQuestion > questionObjectArr.length){
+        endingSequence();
+        
+    }
+    // this will output the question and answers to the card
+    // each answer has a data-correct class to indicate if it is correct
     addQuizItems(questionObjectArr[currentQuestion]);
+    // listens for button clicks on the card
     cardEl.addEventListener("click", (event) => {
+        // checks if an answer button was pressed
         if(event.target.matches("button")){
-            console.log(event.target);
-            console.log(event.target.getAttribute("data-correct"));
+            // checks the correctness of answer button
             if(event.target.getAttribute("data-correct") === "true"){
                 cardTextEl.textContent = "Correct!";
+                removeQuizItems(questionObjectArr[currentQuestion]);
+                // currentQuestion++;
+                // quizGameRound();
+
             }
             else{
-                console.log("inside else");
                 cardTextEl.textContent = "Incorrect";
+                removeQuizItems(questionObjectArr[currentQuestion]);
+                // currentQuestion++;
+                // quizGameRound();
             }
 
         }
@@ -51,6 +66,7 @@ function quizGameRound() {
 
 };
 
+// outputs stored question and answer groups
 function addQuizItems(question){
     // adds the question
     cardTextEl.textContent = question.quest;
@@ -69,6 +85,18 @@ function addQuizItems(question){
         };
         cardEl.children[1].appendChild(newBtn);
     }
+};
+
+// removes answer buttons from the card
+function removeQuizItems(question){
+    // removes each from last added to first
+    for(let i = (question.answers.length - 1); i >= 0; i--){
+        cardEl.children[1].children[i].remove();
+    }
+}
+
+function endingSequence(){
+    return "";
 }
 
 // makes a new start button and listener
@@ -79,4 +107,4 @@ function placeStartBtn(){
     startBtnEl.setAttribute("id", "startBtn");
     starterBlockEl.appendChild(startBtnEl);
     startBtnEl.addEventListener("click", runTimer);
-}
+};
