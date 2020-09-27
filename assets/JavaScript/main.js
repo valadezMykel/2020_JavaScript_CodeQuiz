2c6bd254e061da5d9945db60a4ffc626fb66514d
Mykel Valadez <valadezmykel@gmail.com>
2020-09-27 12:34:40 -0700

    set up and end to the quiz

--- assets/JavaScript/main.js
+++ assets/JavaScript/main.js
@@ -7,14 +7,15 @@ var question1 = {quest: "Who are you?", answers: ["you", "they", "me", "we"], co
 let question2 ={quest: "How many days was JavaScript made in?", answers: ["7", "10", "18", "35"], correctAnswer: 1 };
 var questionObjectArr = [question1, question2]; 
 
-var timer = 5;
+var timer = 15;
 let currentQuestion = 0;
 let score = 0;
 
 // listens at the start button to start the game timer
 document.getElementById("startBtn").addEventListener("click", runTimer);
 
-
+// starts the timer and quizGameRound, removes start button
+// resets timer, currentQuestion, and start button when timer ends
 function runTimer(){
     // removes the start button
     starterBlockEl.removeChild(starterBlockEl.children[1]);
@@ -26,43 +27,47 @@ function runTimer(){
         if(timer < 0){
             clearInterval(timerInterval)
             placeStartBtn()
-            timer = 5;
+            timer = 15;
+            currentQuestion = 0;
             starterBlockEl.children[0].textContent = "You have failed the quiz!!"
 
         }
         
     }, 1000)
 }
-
+console.log(questionObjectArr.length);
 function quizGameRound() {
-    if(currentQuestion > questionObjectArr.length){
-        endingSequence();
+    if(currentQuestion < questionObjectArr.length){
         
+        // this will output the question and answers to the card
+        // each answer has a data-correct class to indicate if it is correct
+        addQuizItems(questionObjectArr[currentQuestion]);
+        // listens for button clicks on the card
+        cardEl.addEventListener("click", (event) => {
+            // checks if an answer button was pressed
+            if(event.target.matches("button")){
+                // checks the correctness of answer button
+                if(event.target.getAttribute("data-correct") === "true"){
+                    cardTextEl.textContent = "Correct!";
+                    removeQuizItems(questionObjectArr[currentQuestion]);
+                    currentQuestion++;
+                    quizGameRound();
+    
+                }
+                else{
+                    cardTextEl.textContent = "Incorrect";
+                    removeQuizItems(questionObjectArr[currentQuestion]);
+                    currentQuestion++;
+                    quizGameRound();
+                }
+    
+            }
+        });
     }
-    // this will output the question and answers to the card
-    // each answer has a data-correct class to indicate if it is correct
-    addQuizItems(questionObjectArr[currentQuestion]);
-    // listens for button clicks on the card
-    cardEl.addEventListener("click", (event) => {
-        // checks if an answer button was pressed
-        if(event.target.matches("button")){
-            // checks the correctness of answer button
-            if(event.target.getAttribute("data-correct") === "true"){
-                cardTextEl.textContent = "Correct!";
-                removeQuizItems(questionObjectArr[currentQuestion]);
-                // currentQuestion++;
-                // quizGameRound();
-
-            }
-            else{
-                cardTextEl.textContent = "Incorrect";
-                removeQuizItems(questionObjectArr[currentQuestion]);
-                // currentQuestion++;
-                // quizGameRound();
-            }
-
-        }
-    });
+    else{
+        endingSequence();
+    }
+    
 
 };
 
@@ -96,7 +101,7 @@ function removeQuizItems(question){
 }
 
 function endingSequence(){
-    return "";
+    alert("the game is over");
 }
 
 // makes a new start button and listener
